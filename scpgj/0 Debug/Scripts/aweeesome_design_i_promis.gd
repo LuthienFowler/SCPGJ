5ace6029b9f977d8a57a02fb8fabf19dcf5aaacd
Luth <noafowl@gmail.com>
2025-03-30 14:30:55 -0600

    Got a very simple navigation system working
    I'll clean it up soon but at this point I just need to get things down

---
+++ scpgj/0 Debug/Scripts/aweeesome_design_i_promis.gd
@@ -0,0 +1,7 @@
+extends ColorRect
+
+#Get the current view of the sprite (possibly a number)
+#Multiply a certain number with the screen width
+
+@export var max_views = 2
+@export var current_view = 1
---
+++ scpgj/0 Debug/Scripts/background.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@onready var right: Button = $Arrows/Right
+@onready var left: Button = $Arrows/Left
+@onready var aweeesome_design_i_promis: ColorRect = $AweeesomeDesignIPromis
+
+func _on_right_pressed() -> void:
+	if aweeesome_design_i_promis.current_view == 1:
+		print("You went right.")
+		aweeesome_design_i_promis.current_view = 2
+		aweeesome_design_i_promis.position.x = -1152
+	elif aweeesome_design_i_promis.current_view == 2:
+		print("You can't go right anymore.")
+
+func _on_left_pressed() -> void:
+	if aweeesome_design_i_promis.current_view == 1:
+		print("You can't go left anymore.")
+	elif aweeesome_design_i_promis.current_view == 2:
+		print("You went left.")
+		aweeesome_design_i_promis.position.x = 0
+		aweeesome_design_i_promis.current_view = 1
+		
