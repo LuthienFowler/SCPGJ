c3825776b90a026230b3554707e60e0a48bdfd49
Luth <noafowl@gmail.com>
2025-04-02 22:03:59 -0600

    Deleted the mouse detection areas
    Gotta look for another solution
    
    I don't wanna use buttons but it looks like I might have to

--- scpgj/1 Interactables/Base/interactable.gd
+++ scpgj/1 Interactables/Base/interactable.gd
@@ -9,9 +9,3 @@ func _input(event: InputEvent) -> void:
 	
 	if Input.is_action_just_pressed("Interact"):
 		print("you have clicked \"" + interactable_name + "!\"")
-
-func _on_area_2d_mouse_entered() -> void:
-	cursor_in_area = true
-
-func _on_area_2d_mouse_exited() -> void:
-	cursor_in_area = false
