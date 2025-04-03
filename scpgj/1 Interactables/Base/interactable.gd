05c73bdfbaad7b7cbfa580ea5f3386a055b134c4
Luth <noafowl@gmail.com>
2025-04-02 22:17:28 -0600

    Figured out the whole interaction thing
    I DON'T HAVE TO USE BUTTONS LETS FUCKING GOOOOO

--- scpgj/1 Interactables/Base/interactable.gd
+++ scpgj/1 Interactables/Base/interactable.gd
@@ -2,10 +2,8 @@ extends Node2D
 class_name Interactable
 
 @export var interactable_name = " "
-var cursor_in_area = false
 
-func _input(event: InputEvent) -> void:
-	if !cursor_in_area: return
-	
-	if Input.is_action_just_pressed("Interact"):
-		print("you have clicked \"" + interactable_name + "!\"")
+
+func _on_areas_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("interact"):
+		print("You've interacted with " + interactable_name + "!")
