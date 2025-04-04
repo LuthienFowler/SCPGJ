6a6244567182443460dc874844fca83ef1b8c5cf
Luth <noafowl@gmail.com>
2025-04-03 22:06:48 -0600

    Adding a bit of dialogue in the playground
    I may be procrastinating making the inventory a little bit lol

--- scpgj/0 Debug/Scripts/interaction_handler.gd
+++ scpgj/0 Debug/Scripts/interaction_handler.gd
@@ -1,8 +1,24 @@
 extends Node
 
+var pink_square_interactions = 0
+var blue_square_interactions = 0
+
 func _ready() -> void:
 	Interactions.interacted.connect(interacted)
 
 func interacted(interactable_name) -> void:
-	if interactable_name == "Pink Square":
+	match interactable_name:
+		"Pink Square":
+			pink_square_dialogue()
+		"Blue Square":
+			blue_square_dialogue()
+
+func pink_square_dialogue():
+	if pink_square_interactions == 0:
 		Interactions.run_dialogue("PinkSquare1")
+	pink_square_interactions += 1
+
+func blue_square_dialogue():
+	if blue_square_interactions == 0:
+		Interactions.run_dialogue("BlueSquare1")
+	blue_square_interactions += 1 
