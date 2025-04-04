219140ad9334f0efca13bdd430aa6809afd883ff
Luth <noafowl@gmail.com>
2025-04-03 23:24:56 -0600

    Organization

--- scpgj/0 Debug/Scripts/background.gd
+++ scpgj/0 Debug/Scripts/background.gd
@@ -2,25 +2,12 @@ extends Node
 
 @onready var right: Button = $Arrows/Right
 @onready var left: Button = $Arrows/Left
-@onready var aweeesome_design_i_promis: ColorRect = $AweeesomeDesignIPromis
 
 func _on_right_pressed() -> void:
 	if Global.current_state != Global.game_states.EXPLORE: return
-	
-	if aweeesome_design_i_promis.current_view == 1:
-		print("You went right.")
-		aweeesome_design_i_promis.current_view = 2
-		aweeesome_design_i_promis.position.x = -1152
-	elif aweeesome_design_i_promis.current_view == 2:
-		print("You can't go right anymore.")
+	print("Go right")
 
 func _on_left_pressed() -> void:
 	if Global.current_state != Global.game_states.EXPLORE: return
-	
-	if aweeesome_design_i_promis.current_view == 1:
-		print("You can't go left anymore.")
-	elif aweeesome_design_i_promis.current_view == 2:
-		print("You went left.")
-		aweeesome_design_i_promis.position.x = 0
-		aweeesome_design_i_promis.current_view = 1
+	print("Go left")
 		
